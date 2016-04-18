6994541362294b3bfeb72c1493898058afb6cd28
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-04-18 00:21:41 -0700

    Mon 04/18/2016  0:21:41.37 JPG

--- run_continuous.bat
+++ run_continuous.bat
@@ -1,9 +1,22 @@
 @echo off
+set GIT_PATH="C:\Program Files\Git\bin\git.exe"
+set BRANCH = "origin gh-pages"
+
 :loop
 color cf
-run_commit.bat
+
+rem java -jar schedsim2016.jar calb
+java -jar schedsim2016.jar idbo
+rem java -jar schedsim2016.jar nvlv
+rem java -jar schedsim2016.jar cave
+
+%GIT_PATH% add -A
+%GIT_PATH% commit -am "%date% %time% JPG"
+%GIT_PATH% pull %BRANCH%
+%GIT_PATH% push %BRANCH%
+
 color 2f
 echo Waiting 60 seconds to update!
-sleep 60
+sleep 10
 echo Updating...
 GOTO loop
