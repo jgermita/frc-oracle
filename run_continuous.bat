c12a33fd5cfe788201ab748bd915ed5d9d3b4071
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-04-18 00:23:40 -0700

    Mon 04/18/2016  0:23:40.17 JPG

--- run_continuous.bat
+++ run_continuous.bat
@@ -16,7 +16,7 @@ rem java -jar schedsim2016.jar cave
 %GIT_PATH% push %BRANCH%
 
 color 2f
-echo Waiting 60 seconds to update!
-sleep 10
+echo Waiting 10 minutes seconds to update!
+sleep 600
 echo Updating...
 GOTO loop
