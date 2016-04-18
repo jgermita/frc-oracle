e92bcf6d4305bc3cb9f5d0dd90f2a69053c6e7d2
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-04-18 01:38:23 -0700

    Mon 04/18/2016  1:38:23.60 JPG

--- run_commit.bat
+++ run_commit.bat
@@ -2,10 +2,14 @@
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin gh-pages"
 
-java -jar schedsim2016.jar calb
-java -jar schedsim2016.jar idbo
-java -jar schedsim2016.jar nvlv
-java -jar schedsim2016.jar cave
+java -jar schedsim2016.jar arc
+java -jar schedsim2016.jar cur
+java -jar schedsim2016.jar gal
+java -jar schedsim2016.jar new
+java -jar schedsim2016.jar cars
+java -jar schedsim2016.jar carv
+java -jar schedsim2016.jar hop
+java -jar schedsim2016.jar tes
 
 %GIT_PATH% add -A
 %GIT_PATH% commit -am "%date% %time% JPG"
