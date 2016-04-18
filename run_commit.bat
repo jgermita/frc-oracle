b7334b477e48a16327da397d9d35af5daf9a8905
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-04-17 23:11:30 -0700

    Sun 04/17/2016 23:11:30.52 JPG

--- run_commit.bat
+++ run_commit.bat
@@ -2,8 +2,10 @@
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin gh-pages"
 
+java -jar schedsim2016.jar idbo
+
 %GIT_PATH% add -A
-%GIT_PATH% commit -am "Auto-committed on %date%"
+%GIT_PATH% commit -am "%date% %time% JPG"
 %GIT_PATH% pull %BRANCH%
 %GIT_PATH% push %BRANCH%
 
