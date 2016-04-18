fbf3f27bcd6a50cccbd825f6adfde86489d0b766
Jeremy Paul Germita <jeremy.germita@gmail.com>
2016-04-17 22:52:21 -0700

    Sun 04/17/2016 22:52:21.88 JPG

--- run_commit.bat
+++ run_commit.bat
@@ -3,7 +3,7 @@ cd "C:\Users\jeremy\Documents\GitHub\frc-oracle"
 REM run.bat
 echo committing
 git add *
-git commit -m "%date %time% JPG"
+git commit -m "%date% %time% JPG"
 echo pushing
 git push origin gh-pages
 echo done. 
